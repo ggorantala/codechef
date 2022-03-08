994da3827b017616ab16ccf66b3dc4c25cb4059c
ggorantala <gopishiva001@outlook.com>
2022-03-08 17:15:51 +0530

    Removed a comment

--- src/learndsa01/IsBoth.java
+++ src/learndsa01/IsBoth.java
@@ -2,7 +2,6 @@ package learndsa01;/* package codechef; // don't place package name! */
 
 import java.util.Scanner;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class IsBoth {
   public static void main(String[] args) throws java.lang.Exception {
     // your code goes here
